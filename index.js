d5361fa8150f7ad08ed971eb73c08a3ae940d534
CalinR <calin.reimer@gmail.com>
2017-08-17 22:30:32 -0500

    implements CLI

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
+#! /usr/bin/env node
 const fs = require('fs');
-const config = require('./config.json');
+const program = require('commander');
 const Shopify = require('shopify-api-node');
 const parse = require('csv-parse');
 const ProgressBar = require('progress');
+let filename;
+let apiKey;
+let password;
+let shop;
 let csvData = [];
+
+program.arguments('<file>')
+    .option('-a, --apikey <apikey>', 'The Shopify API Key')
+    .option('-p, --password <password>', 'The Shopify API Password')
+    .option('-s, --shop <shop>', 'The Shopify Shop Name')
+    .action(function(file) {
+        filename = file;
+        apiKey = program.apikey;
+        password = program.password;
+        shop = program.shop;
+    }).parse(process.argv);
+
+if(!filename){
+    console.error('filename is required');
+    process.exit(1);
+}
+if(!apiKey){
+    console.error('apikey is required');
+    process.exit(1);
+}
+if(!password){
+    console.error('password is required');
+    process.exit(1);
+}
+if(!shop){
+    console.error('shop is required');
+    process.exit(1);
+}
+
 const shopify = new Shopify({
-  shopName: config.shop,
-  apiKey: config.api_key,
-  password: config.password,
+  shopName: shop,
+  apiKey: apiKey,
+  password: password,
   autoLimit: true
 });
-
-fs.createReadStream(config.csv_name)
-    .pipe(parse({delimiter: ',', columns: true }))
-    .on('data', function(csvrow) {
-        csvData.push(csvrow);        
-    })
-    .on('end',function() {
-        uploadCollections();
-    });
 
 function uploadCollections(){
     const bar = new ProgressBar('importing: :current/:total [:bar] :percent', {
@@ -29,9 +54,8 @@ function uploadCollections(){
     })
 
     for(const [index, row] of csvData.entries()){
-        shopify.smartCollection.create({
+        let data = {
             title: row.Title,
-            body_html: row.Description,
             rules: [
                 {
                     "column": row.Column,
@@ -39,7 +63,21 @@ function uploadCollections(){
                     "condition": row.Condition
                 }
             ]
-        }).then(function(){
+        };
+
+        if(row.Description){
+            data.body_html = row.Description;
+        }
+        if(row.Handle){
+            data.handle = row.Handle;
+        }
+        if(row.Image){
+            data.image = {
+                src: row.Image
+            }
+        }
+
+        shopify.smartCollection.create(data).then(function(){
             if (bar.complete) {
                 console.log('\ncomplete\n');
             }
@@ -49,3 +87,12 @@ function uploadCollections(){
         })
     }
 }
+
+fs.createReadStream(filename)
+    .pipe(parse({delimiter: ',', columns: true }))
+    .on('data', function(csvrow) {
+        csvData.push(csvrow);        
+    })
+    .on('end',function() {
+        uploadCollections();
+    });
